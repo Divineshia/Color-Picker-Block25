392cc3ae01942b99eaa861f3f720f748d6647592
Divineshia Sharon <divine@Divineshias-Air.lan>
2023-07-25 13:31:16 -0500

    selected color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-// Write your Color component here
+import {useState} from "react";
 
+
+// Write your Color component here
+const Color= ({color,setSelectedColor,setColor})=>{
+  return (<div className= {color}
+  onClick={()=>setSelectedColor(color)}>
+  </div>
+  )
+}
+// //Or Object deconstruction
+// const Color =({color})=>{
+//   return <div className={color}></div>
+// }
 const App = () => {
+  const [selectedColor, setSelectedColor]=useState('')
+  //console.log(selectedColor);
   return (
     <div id="container">
       <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className>Currently selected: </div>
+        <div className ={selectedColor}>{`selectedColor ${selected}` }</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color ='yellow' setSelectedColor={setSelectedColor} setColor='selected' />
+        <Color color='green' setSelectedColor={setSelectedColor} setColor='selected'/>
+        <Color color ='violet'setSelectedColor={setSelectedColor}setColor='selected'/>
+</div>
     </div>
   );
 };
